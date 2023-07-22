de389a64fc624c795a8d1f6ed01ad476189c72b6
Aleksey <103183090+Vlasvin@users.noreply.github.com>
2023-07-22 15:48:16 +0300

    delete html cod

--- js/test.js
+++ js/test.js
@@ -1,3 +1,23 @@
-const quantity = prompt(`Input quantity of goods`);
-console.log(quantity);
-console.log(typeof quantity);
+// const quantity = prompt(`Input quantity of goods`);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+const value = 4;
+let msg = '';
+if (value >= 10) {
+  msg = more;
+} else {
+  msg = less;
+}
+console.log(msg);
+
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+for (let i = 0; i < i.length - 1; i += 1) {
+  if (i === 'plum') {
+    fruits['plum'] = 'peach';
+  } else if (i === 'orange') {
+    fruits['orange'] = 'banana';
+  }
+}
+console.log(fruits);
+Clg;
